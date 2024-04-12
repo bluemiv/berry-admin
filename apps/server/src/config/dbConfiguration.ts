e2a4a3548a615194e0e921e4939d9de7b5b1e7a2
taehongkim <public.bluemiv@gmail.com>
2024-04-12 15:14:54 +0900

    feat: server 와 db 설정

---
+++ apps/server/src/config/dbConfiguration.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmModuleOptions: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'root',
+  password: 'root',
+  database: 'berry-dashboard-db',
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: true,
+};
