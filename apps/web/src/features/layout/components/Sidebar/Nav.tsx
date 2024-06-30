45f7a8c2832d4b93bcd8c319ff4b6d8910b751ce
taehongkim <public.bluemiv@gmail.com>
2024-06-30 20:59:03 +0900

    feat: navigation active 설정

--- apps/web/src/features/layout/components/Sidebar/Nav.tsx
+++ apps/web/src/features/layout/components/Sidebar/Nav.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import { ROUTE_PATH } from '@/routes';
 import NavItem from './NavItem';
 
 const Nav = () => {
+  const location = useLocation();
+
   return (
     <nav className="w-full">
       <ul className="flex flex-col gap-md w-full">
@@ -10,9 +13,10 @@ const Nav = () => {
           { label: 'Dashboard', href: ROUTE_PATH.DASHBOARD },
           { label: 'User', href: ROUTE_PATH.USER },
           { label: 'Product', href: ROUTE_PATH.PRODUCT },
-        ].map(({ label, href }) => (
-          <NavItem key={href} label={label} href={href} />
-        ))}
+        ].map(({ label, href }) => {
+          const isActive = new RegExp(location.pathname).test(href);
+          return <NavItem key={href} label={label} href={href} isActive={isActive} />;
+        })}
       </ul>
     </nav>
   );
--- apps/web/src/features/layout/components/Sidebar/NavItem.tsx
+++ apps/web/src/features/layout/components/Sidebar/NavItem.tsx
@@ -1,17 +1,22 @@
+import classNames from 'classnames';
 import React, { ReactNode } from 'react';
 import { Link } from 'react-router-dom';
 
 interface TProps {
   label: ReactNode;
   href: string;
+  isActive: boolean;
 }
 
-const NavItem = ({ label, href }: TProps) => {
+const NavItem = ({ label, href, isActive }: TProps) => {
   return (
     <li className="w-full">
       <Link
         to={href}
-        className="flex items-center rounded py-xs px-lg w-full hover:bg-primary hover:text-white transition ease-in-out duration-150"
+        className={classNames(
+          'flex items-center rounded py-xs px-lg w-full transition ease-in-out duration-150',
+          isActive ? 'bg-primary text-white ' : 'hover:bg-primary hover:text-white',
+        )}
       >
         {label}
       </Link>
