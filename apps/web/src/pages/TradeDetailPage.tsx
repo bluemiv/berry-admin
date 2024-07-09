5e75aefee3650fa8e1a6e2efd8c9d6f668c07970
taehongkim <public.bluemiv@gmail.com>
2024-07-09 22:24:29 +0900

    feat: trade, trade history 기능 개발

--- apps/web/src/pages/TradeDetailPage.tsx
+++ apps/web/src/pages/TradeDetailPage.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Card } from 'antd';
-import { useParams } from 'react-router-dom';
+import { Button, Card } from 'antd';
+import { useNavigate, useParams } from 'react-router-dom';
 import { TradeHistoryTable, TradeHistoryTableActions } from '@/features/trade';
 
 const TradeDetailPage = () => {
+  const nav = useNavigate();
+
   const params = useParams();
   const tradeId = Number(params.tradeId);
 
   return (
     <main className="p-md h-contents flex flex-col gap-md">
+      <div>
+        <Button onClick={() => nav(-1)}>뒤로가기</Button>
+      </div>
       <Card title="트레이드 상세 목록">
         <div className="flex flex-col gap-md">
           <TradeHistoryTableActions tradeId={tradeId} />
